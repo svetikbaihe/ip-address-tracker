ac46e4ab22b39ac972a4f9341212dcc1c9ec94af
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-08-12 21:32:19 +0900

    feat: spinner

---
+++ src/elements/Spinner/types.ts
@@ -0,0 +1,13 @@
+export interface SpinnerInterface{
+  spinnerElement: HTMLElement | null
+}
+
+export interface SpinnerConstructor{
+  size: SpinnerSize
+}
+
+export type SpinnerSize = 
+'small' |
+'medium' |
+'large' |
+'page'
