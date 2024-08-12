03d62278b14514e07aed300f41f88242c6899472
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-08-12 14:55:37 +0900

    feat: button

---
+++ src/elements/Button/component.ts
@@ -0,0 +1,37 @@
+import styles from './styles.module.scss';
+import type { ButtonConstructor, ButtonInterface } from "./types";
+
+class Button implements ButtonInterface {
+  protected handleClick: VoidFunction = () => {};
+  protected $button: HTMLElement | null = null;
+
+  constructor ({ onClick }: ButtonConstructor) {
+    if (onClick) {
+      this.handleClick = onClick
+    }
+
+    this.buildButton();
+  }
+
+  public get buttonElement() {
+    return this.$button;
+  }
+
+  protected buildButton = () => {
+    const $button = document.createElement('button');
+
+    $button.className = styles.button;
+
+    $button.addEventListener('click', () => this.handleClick());
+
+    const $buttonIcon = document.createElement('img');
+
+    $buttonIcon.setAttribute('src', '/icons/icon-arrow.svg');
+
+    $button.appendChild($buttonIcon);
+
+    this.$button = $button;
+  }
+}
+
+export default Button;
