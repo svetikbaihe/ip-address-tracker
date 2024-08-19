45958f4056486afa4c5ac8dc643e433d902b6800
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-08-19 21:42:22 +0900

    Fix the API key and images

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
 export const API_METHOD_GET: string = 'get';
 
-export const API_HOST: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_GEO_API_KEY}`;
+export const API_HOST: string = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_nVIwVXCZr9HkBg585TFzEzmP03v2y';
